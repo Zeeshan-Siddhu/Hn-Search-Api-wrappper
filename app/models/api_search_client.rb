2c6d9377d217ae8b3cc2c2c82ee6a0860ee99783
Zeeshan Mahmood <zeeshan.mahmood@7vals.com>
2015-11-17 03:29:12 +0500

    change url to https to support heroku production environment

--- app/models/api_search_client.rb
+++ app/models/api_search_client.rb
@@ -4,7 +4,7 @@ class ApiSearchClient
   #total count used for pagination
   def self.total_count_of_filtered_items
     begin 
-      response = HTTParty.get('http://hn.algolia.com/api/v1/search_by_date?tags=story&query=github&restrictSearchableAttributes=url&numericFilters=points>1000')
+      response = HTTParty.get('https://hn.algolia.com/api/v1/search_by_date?tags=story&query=github&restrictSearchableAttributes=url&numericFilters=points>1000')
       response['hits'].size
     rescue => e
       0
@@ -15,7 +15,7 @@ class ApiSearchClient
   #api call with conditional filters
   def self.request_items page
     begin
-      response = HTTParty.get("http://hn.algolia.com/api/v1/search_by_date?tags=story&query=github&restrictSearchableAttributes=url&numericFilters=points>1000&hitsPerPage=5&page=#{page}")
+      response = HTTParty.get("https://hn.algolia.com/api/v1/search_by_date?tags=story&query=github&restrictSearchableAttributes=url&numericFilters=points>1000&hitsPerPage=5&page=#{page}")
       response['hits']
     rescue => e
       []
